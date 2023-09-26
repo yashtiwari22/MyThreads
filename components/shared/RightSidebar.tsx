423ecf8067f60c6ae356f75262d2ea6a4690fb04
Yash Tiwari <yash22012002@gmail.com>
2023-09-27 00:38:03 +0530

    threads clone

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,22 @@
+import { currentUser } from "@clerk/nextjs";
+
+async function RightSidebar() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Similar Minds</h3>
+      </div>
+    </section>
+  );
+}
+
+export default RightSidebar;
